497080b964bf0de648d4a7b3d1a9c1fdf1486067
archivojab <archivojab@361e9262-27e0-11df-bfd4-992fa37f6226>
2010-05-08 18:44:02 +0000

    Code

---
+++ nxt/PED2009/20100508/jungleking/src/RobotZebra.java
@@ -0,0 +1,74 @@
+import lejos.nxt.*;
+/**
+ *
+ * @author User Alberto Rodriguez
+ *
+ */
+public class RobotZebra {
+    /**
+     * Area de propiedades del robot
+     */
+    private Motor leftMotor;
+    private Motor rightMotor;
+ 
+    /**
+     *
+     */
+ 
+    public RobotZebra(){
+        leftMotor = Motor.A;
+        rightMotor= Motor.B;
+    }
+ 
+    public void forward(int miliseconds){
+        leftMotor.forward();
+        rightMotor.forward();
+        try{Thread.sleep(miliseconds);}catch(Exception e) {}
+    }
+    public void backward(int miliseconds){
+        leftMotor.backward();
+        rightMotor.backward();
+        try{Thread.sleep(miliseconds);}catch(Exception e) {}
+    }
+ 
+    public void stop(){
+        rightMotor.stop();
+        leftMotor.stop();
+    }
+ 
+    public void turnLeft(int angle){
+        leftMotor.rotate(angle);
+    }
+    public void turnRight(int angle){
+        rightMotor.rotate(angle);
+    }
+ 
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        RobotZebra RobotZebra = new RobotZebra();
+ 
+        Boolean detectado = false;
+        while(!Button.ESCAPE.isPressed()){
+            
+        	RobotZebra.forward(10000);
+        	detectado = detectarLeon();
+        	if(detectado){
+                //RobotZebra.stop();
+                //RobotZebra.turnRight(720);
+                RobotZebra.stop();
+                RobotZebra.backward(10000);
+        	}else{
+            	detectado = detectarLeon();
+        	}
+            //RobotZebra.stop();
+            //RobotZebra.turnRight(720);
+            RobotZebra.stop();
+            RobotZebra.backward(10000);
+        }
+        //RobotZebra.turnRight(180);
+    }
+ 
+}
