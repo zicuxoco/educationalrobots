621a8d29220c829594c888f16871aa557d8d3947
archivojab <archivojab@361e9262-27e0-11df-bfd4-992fa37f6226>
2010-06-13 11:20:50 +0000

    Mira justo, te estoy enseñando como hacer comentarios en los codigos fuentes. Miratelo.

--- trunk/java/JavaStepByStep/chapter1/justo/helloWorld/src/HelloWorld.java
+++ trunk/java/JavaStepByStep/chapter1/justo/helloWorld/src/HelloWorld.java
@@ -1,5 +1,19 @@
 
+/**
+ * Este es el primer programa de Justo.
+ * Esta empleando herramientas colaborativas para trabajar en equipo. 
+ * 
+ * @author Justo Araque Lopez
+ * @author Juan Antonio Brenha Moral
+ *
+ */
 public class HelloWorld {
+	
+	/**
+	 * Este es el metodo principal de todo proyecto Java
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
