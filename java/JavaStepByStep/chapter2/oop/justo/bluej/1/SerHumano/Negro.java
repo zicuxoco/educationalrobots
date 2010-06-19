3d7965fdc76827d86cb4cb69133f248629f24935
justo.araque.lopez <justo.araque.lopez@361e9262-27e0-11df-bfd4-992fa37f6226>
2010-06-19 15:11:33 +0000

    El trabajo de la semana del 12 al 19 de Junio

---
+++ java/JavaStepByStep/chapter2/oop/justo/bluej/1/SerHumano/Negro.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Negro here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Negro extends SerHumano
+{
+
+    private String raza = "Negrata";
+
+    /**
+     * Constructor for objects of class Negro
+     */
+    public Negro(String _sexo)
+    {
+        super(_sexo,"negrata");
+    }
+
+}
