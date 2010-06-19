2ebb6c318456b82fa56ba4674440d7e024bb3fd4
archivojab <archivojab@361e9262-27e0-11df-bfd4-992fa37f6226>
2010-06-19 23:05:06 +0000

    Testing SubEclipse

--- trunk/java/JavaStepByStep/chapter1/justo/helloWorld/src/HelloWorld.java
+++ trunk/java/JavaStepByStep/chapter1/justo/helloWorld/src/HelloWorld.java
@@ -17,5 +17,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
+		//Probando SVN desde Eclipse
 	}
 }
