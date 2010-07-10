a5a393547dce52200c54c38db41d6fab5f729972
archivojab <archivojab@361e9262-27e0-11df-bfd4-992fa37f6226>
2010-07-10 08:04:33 +0000

    Preliminar version of SSL which return values from sensor.

--- nxt/LeJOS/labs/catcan/ssl/src/SSLTest.java
+++ nxt/LeJOS/labs/catcan/ssl/src/SSLTest.java
@@ -8,7 +8,7 @@ public class SSLTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SSL SSLObj = new SSL(SensorPort.S1);
-		//SSLObj.setFilterON();
+		SSLObj.setFilterON();
 		
 		while(!Button.ESCAPE.isPressed()){
 			SSLObj.get3DVectors();
